a55389e7e4778db99fb16359e1802917f8b20d0b
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-01 00:50:20 +0100

    updates

--- src/HamBurger.java
+++ src/HamBurger.java
@@ -1,2 +1,13 @@
 public class HamBurger {
+
+    private String type;
+    private double basePrice;
+    private int toppings;
+
+    public HamBurger(String type, double basePrice) {
+        this.type = type;
+        this.basePrice = basePrice;
+        this.toppings = 3;
+
+    }
 }
