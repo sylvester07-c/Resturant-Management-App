f327d225956ad7bc5758d745aeea976902eed823
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-01 01:53:42 +0100

    updates

--- src/SideItem.java
+++ src/SideItem.java
@@ -1,2 +1,10 @@
 public class SideItem {
+    private String type;
+    private String size;
+
+    public SideItem(String type, String size) {
+        this.type = type;
+        this.size = size;
+    }
+
 }
